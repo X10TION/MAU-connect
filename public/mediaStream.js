0be82e2a78e72fb83c65df188ac70086249fb65b
X10tion <x10tion007@gmail.com>
2022-05-26 21:04:18 -0700

    video display

--- public/mediaStream.js
+++ public/mediaStream.js
@@ -1,1 +1,58 @@
 const socket = io()
+const key = document.getElementById("KEY")
+const localvideo1 = document.getElementById("localvideo")
+const call = document.getElementById("call")
+const stop = document.getElementById("stop")
+const Mute = document.getElementById("Mute")
+const Unmute = document.getElementById("Unmute")
+const End = document.getElementById("End")
+let tracks = []
+socket.on('connect', () => {
+    localStorage.setItem("private", socket.id)
+    key.value = socket.id
+    console.log(socket.id)
+})
+// get local media devices
+const openMediaDevice = async() =>{
+    try{
+        let stream = await navigator.mediaDevices.getUserMedia({video:true,audio:true})
+        localvideo1.srcObject = stream
+        tracks = stream.getTracks()
+    } catch(error){
+        console.log(error)
+    }
+}
+
+// START FUNCTION
+call.addEventListener('click',() =>{
+    openMediaDevice()
+    Mute.addEventListener('click',muteTrack)
+    End.addEventListener('click', stopTrack)
+
+})
+
+
+
+// // using mediaDevice
+// navigator.mediaDevices.getUserMedia({video:true,audio:true})
+// .then(stream => {
+//     console.log("stream2", stream)
+//     let tracks = stream.getTracks()
+//  tracks.forEach(track => stream1.addTrack(track))
+    
+// })
+
+// mute tack
+
+const muteTrack = () => {
+    tracks.forEach(track => track.enabled = false)
+     Unmute.addEventListener('click',unMuteTracks)
+}
+// unmute tracks
+const unMuteTracks = () => {
+    tracks.forEach( track => track.enabled = true)
+}
+//  stop lecture
+const stopTrack = () => {
+    tracks.forEach(track => track.stop())
+}
